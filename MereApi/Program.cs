90815bf73a00887d96309be0f69af4f1c69e8b93
Scott W. Vincent <scott@swvincent.com>
2025-01-28 07:44:04 -0500

    Very simple implementation of BibleUtil in API

--- MereApi/Program.cs
+++ MereApi/Program.cs
@@ -3,4 +3,11 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/bible/{book}", (string reference) =>
+{
+    var r = BibleUtil.Reference.Parse(reference);
+
+    return r.ToString();
+});
+
 app.Run();
