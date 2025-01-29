a0eed79c4106b3d18eec7743908b3cc76228f55e
Scott W. Vincent <scott@swvincent.com>
2025-01-28 21:56:02 -0500

    More work with API

--- MereApi/Program.cs
+++ MereApi/Program.cs
@@ -1,13 +1,33 @@
+using BibleUtil;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using System.Net;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddProblemDetails();
+
 var app = builder.Build();
+
+// TODO: Disable this for dev
+// if(!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler();
+}
+app.UseStatusCodePages();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/bible/{book}", (string reference) =>
+app.MapGet("/bible/{reference}", Results<NotFound<string>, Ok<string>> (string reference) =>
 {
-    var r = BibleUtil.Reference.Parse(reference);
-
-    return r.ToString();
+    try
+    {
+        var r = BibleUtil.Reference.Parse(reference);
+        return TypedResults.Ok(r.ToString());
+    }
+    catch
+    {
+        return TypedResults.NotFound("The reference is not valid");
+    }        
 });
 
 app.Run();
